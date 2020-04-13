799db6114e276ed3e4a8ae6e17cc63a1536c653c
Christine Ramos <christineramos12@gmail.com>
2020-04-12 14:52:53 -1000

    Basic back display outline

--- src/app/back-display/back-display.component.ts
+++ src/app/back-display/back-display.component.ts
@@ -1,25 +1,26 @@
-// import { BackDisplayComponent } from 'app-back-display.component';
+/** TODO: Implement alert interface */
+
 import {SelectionModel} from '@angular/cdk/collections';
 import {Component} from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 
 export interface PeriodicElement {
-  name: string;
+  name: string;   /** Includes Last, First */
   petID: string;
+  /** Arrived, Inspected, Release -- Checkboxes */
 }
 
-/**
- * @title Table with selection
- */
+const ELEMENT_DATA: ownerInfo[] = [];   /** Wherever our data will come from */
+
 @Component({
   selector: 'app-back-display',
   styleUrls: ['back-display.component.css'],
   templateUrl: 'back-display.component.html',
 })
-export class BackDisplayComponent {
+export class {
   displayedColumns: string[] = ['name', 'petID', 'arrived', 'inspected', 'release'];
-  dataSource = new MatTableDataSource<BackDisplayComponent>(this.data);
-  selection = new SelectionModel<BackDisplayComponent>(true, []);
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  selection = new SelectionModel<PeriodicElement>(true, []);  /** For status checkboxes */
 
   /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
@@ -36,7 +37,7 @@ export class BackDisplayComponent {
   }
 
   /** The label for the checkbox on the passed row */
-  checkboxLabel(row?: BackDisplayComponent): string {
+  checkboxLabel(row?: PeriodicElement): string {
     if (!row) {
       return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
     }
